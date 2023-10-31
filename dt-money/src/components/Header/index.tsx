c2365e8a484a3dad15a2abcf3da52d29c3379b7f
Jonatas_Amorim <jonatas231997@live.com>
2023-10-31 16:34:45 -0300

    styled component modal with Dialog

--- dt-money/src/components/Header/index.tsx
+++ dt-money/src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import { NewTransactionModal } from "../NewTransactionModal"
 import * as Dialog from '@radix-ui/react-dialog';
 
 import logoImg from "../../assets/logo.svg";
@@ -17,13 +18,7 @@ export function Header() {
               </NewTransactionButton>
             </Dialog.Trigger>
 
-            <Dialog.Portal>
-              <Dialog.Overlay />
-              <Dialog.Content>
-                <Dialog.Title>Nova Transação</Dialog.Title>
-                <Dialog.Close/>
-              </Dialog.Content>
-            </Dialog.Portal>
+            <NewTransactionModal />
           </Dialog.Root>
         </HeaderContent>
       </HeaderContainer>
