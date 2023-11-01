38a23d2fe2edd99e3a7972efa7fa9b07e3a03080
JonatasAS <jonatas23997@live.com>
2023-10-31 22:06:10 -0300

    creating button transaction with Radius Groud Radix

--- dt-money/src/components/NewTransactionModal/index.jsx
+++ dt-money/src/components/NewTransactionModal/index.jsx
@@ -1,7 +1,7 @@
-import { CloseButton, Content, Overlay } from './styles'
+import { CloseButton, Content, Overlay, TransactionTypeButton, TrasactionType } from './styles'
 
 import * as Dialog from '@radix-ui/react-dialog'
-import { X } from 'phosphor-react'
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
 
 export function NewTransactionModal() {
     return (
@@ -17,6 +17,18 @@ export function NewTransactionModal() {
                     <input type="text" placeholder="Descrição" required/>
                     <input type="number" placeholder="Preço" required/>
                     <input type="text" placeholder="Categoria" required/>
+
+                    <TrasactionType>
+                        <TransactionTypeButton variant='income' value='income'>
+                            <ArrowCircleUp size={24} />
+                            Entrar
+                        </TransactionTypeButton>
+
+                        <TransactionTypeButton variant='outcome' value='outcome'>
+                            <ArrowCircleDown size={24} />
+                            Saída
+                        </TransactionTypeButton>
+                    </TrasactionType>
 
                     <button type='submit'>
                         Cadastrar
