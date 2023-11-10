b3817fccd5f5db9fc197ac34ef2dfa3f9ddb180d
JonatasAS <jonatas23997@live.com>
2023-11-09 23:24:59 -0300

    useForm form search and modal

--- dt-money/src/pages/Transactions/components/SearchForm/index.tsx
+++ dt-money/src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,13 +1,36 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { SearchFormContainer } from "./styles";
 
 import { MagnifyingGlass } from "phosphor-react"
+import { useForm } from "react-hook-form"
+import * as z from 'zod'
+
+const searchFormSchema = z.object({
+    query: z.string()
+})
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
-    return(
-        <SearchFormContainer>
-            <input type="text" placeholder="Busque por transações" />
+    const { register, handleSubmit, formState: {isSubmitting} } = useForm<SearchFormInputs>({
+        resolver: zodResolver(searchFormSchema)
+        })
+    
+    async function handleSearchTransactions(data: SearchFormInputs) {
+        await new Promise(resolve => setTimeout(resolve, 2000))
+        
+        console.log(data)
+    }
 
-            <button>
+    return (
+        <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+            <input 
+                type="text"
+                placeholder="Busque por transações" 
+                {...register('query')}
+            />
+
+            <button type="submit" disabled={isSubmitting}>
                 <MagnifyingGlass size={20} />
                 Buscar
             </button>
